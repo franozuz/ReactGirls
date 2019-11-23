2062ea5b7ce8ae86af6861de277842eaafe2b82f
Zuzana Franova <franova.z@gmail.com>
2019-11-23 16:03:15 +0100

    Change color

--- components/Hello.tsx
+++ components/Hello.tsx
@@ -2,10 +2,11 @@ import React, { FunctionComponent } from "react";
 
 type HelloProps = {
   name: string;
+  color?: string;
 };
 
-const Hello: FunctionComponent<HelloProps> = ({ name }) => (
-  <div style={{color:"violet"}}>
+const Hello: FunctionComponent<HelloProps> = ({ name, color }) => (
+  <div style={{color:color || "orange"}}>
     <h1> Hello {name}</h1>
   </div>
 );
