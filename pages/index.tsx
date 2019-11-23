9c48cf50063b14aa75cc87e20dbf61247592ee5f
Zuzana Franova <franova.z@gmail.com>
2019-11-23 13:28:39 +0100

    Update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,13 @@
 export default () => {
-  return <div>Welcome to Next.js!</div>;
+  return (
+    <div>
+      <h1>React Girls Prague</h1>
+      <p>
+        The Reactgirls Workshop is a friendly and supportive environment for you
+        to develop your tech skills. We passionately empower girls and women in
+        their coding journey by offering this cool, 1-day, free workshop for
+        members of all ages and skills.
+      </p>
+    </div>
+  );
 };
